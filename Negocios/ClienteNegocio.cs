b988c81cd4f7b3b4bafdf1cbb7cd6f9b11234d7c
Gabriel Oliveira <gabriel0211oliveira15@gmail.com>
2026-09-07 22:46:38 -0300

    Implementação da camada de negócio

---
+++ Negocios/ClienteNegocio.cs
@@ -0,0 +1,21 @@
+﻿using AcessoBancoDados;
+using ObjetoTransferencia;
+using System.Data;
+
+namespace Negocios
+{
+    public class ClienteNegocio
+    {
+        public string InserirCliente(Cliente cliente)
+        {
+            AcessoDadosSqlServer acessoDadosSqlServer = new AcessoDadosSqlServer();
+
+            acessoDadosSqlServer.LimparParametros();
+            acessoDadosSqlServer.AdicionarParametros("@Nome", cliente.Nome);
+            acessoDadosSqlServer.AdicionarParametros("@CPF", cliente.CFP);
+            string IDCliente = acessoDadosSqlServer.ExecutarManipulacao(CommandType.StoredProcedure, "uspCadastrarCliente").ToString();
+
+            return IDCliente;
+        }
+    }
+}
---
+++ Negocios/PedidoNegocio.cs
@@ -0,0 +1,19 @@
+﻿using AcessoBancoDados;
+using ObjetoTransferencia;
+using System.Data;
+
+namespace Negocios
+{
+    public class PedidoNegocio
+    {
+        public string InserirPedido(Pedido pedido)
+        {
+            AcessoDadosSqlServer acessoDadosSqlServer = new AcessoDadosSqlServer();
+            acessoDadosSqlServer.LimparParametros();
+            acessoDadosSqlServer.AdicionarParametros("@IDCliente", pedido.Cliente.IDCliente);
+            string IDPedido = acessoDadosSqlServer.ExecutarManipulacao(CommandType.StoredProcedure, "uspCadastrarPedido").ToString();
+
+            return IDPedido;
+        }
+    }
+}
